fcd94b591baf2707983ccfbc6fb8c3f048a719e7
vitorGaia <vitorgaia0027@gmail.com>
2023-09-20 16:07:45 -0300

    finaliza req 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,14 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    count = 0
+
+    if (not isinstance(target_time, int)):
+        return None
+
+    for entry, exit in permanence_period:
+        if not (isinstance(entry, int) and isinstance(exit, int)):
+            return None
+
+        if (entry <= target_time and exit >= target_time):
+            count += 1
+
+    return count
