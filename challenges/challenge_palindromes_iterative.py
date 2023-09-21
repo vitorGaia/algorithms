251334b75aa3c7ba083acbbd530cb0c41ca7f821
vitorGaia <vitorgaia0027@gmail.com>
2023-09-21 14:01:09 -0300

    finaliza projeto.

--- challenges/challenge_palindromes_iterative.py
+++ challenges/challenge_palindromes_iterative.py
@@ -1,3 +1,15 @@
 def is_palindrome_iterative(word):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if word == '':
+        return False
+
+    min_index = 0
+    max_index = len(word) - 1
+
+    while min_index < max_index:
+        if word[min_index] != word[max_index]:
+            return False
+
+        min_index += 1
+        max_index -= 1
+
+    return True
